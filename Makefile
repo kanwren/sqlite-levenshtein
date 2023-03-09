bbc90b88b91bd02046b5feb6d8aab8fd7234c004
Nicole Wren <wrenn@squareup.com>
2023-03-08 22:36:08 -0500

    Minor cleanup

--- Makefile
+++ Makefile
@@ -1,18 +1,21 @@
 SQLITE3_BIN = /opt/homebrew/opt/sqlite3/bin/sqlite3
-LINKER_FLAGS = -Wl,-flat_namespace -Wl,-undefined -Wl,suppress -Wl,-single_module
+LINKER_FLAGS = -dynamiclib -Wl,-flat_namespace -Wl,-undefined -Wl,suppress -Wl,-single_module
+CFLAGS = -Wall -Wextra -O3
 
 all: levenshtein.dylib
 
 levenshtein.dylib: levenshtein.o
-	gcc $(LINKER_FLAGS) -dynamiclib -O2 -o $@ $<
+	gcc $(CFLAGS) $(LINKER_FLAGS) -o $@ $<
 
 levenshtein.o: levenshtein.c
-	gcc -g -O2 -fno-common -DPIC -o $@ -c $<
+	gcc $(CFLAGS) -fno-common -DPIC -o $@ -c $<
 
 .PHONY: clean
 clean:
 	rm levenshtein.o levenshtein.dylib
 
-.PHONY: test
+.PHONY: test check
+check: levenshtein.dylib
+	echo ".load levenshtein" | $(SQLITE3_BIN)
 test: levenshtein.dylib
-	$(SQLITE3_BIN) < ./levenshtein-test.sql
+	time $(SQLITE3_BIN) < ./levenshtein-test.sql
--- levenshtein.c
+++ levenshtein.c
@@ -1,28 +1,42 @@
 #include <sqlite3ext.h>
+SQLITE_EXTENSION_INIT1
 #include <string.h>
 #include <stdlib.h>
 
+#ifdef __GNUC__
+#define UNUSED(x) UNUSED ## x __attribute__((unused))
+#else
+#define UNUSED(x) UNUSED_ ## x
+#endif
+
 #define LEVENSHTEIN_MAX_STRLEN 1024
-// O(n * m) !!!
 
 #define MIN(a,b) (((a) < (b)) ? (a) : (b))
-#define MAX(a,b) (((a) > (b)) ? (a) : (b))
 
-SQLITE_EXTENSION_INIT1
+static void levenshtein(sqlite3_context *context, int argc, sqlite3_value **argv);
+static inline int levenshtein_distance(char const * const restrict s1, char const * const restrict s2);
 
-int levenshtein_distance(char*, char*);
+// Initialize the extension
+int sqlite3_levenshtein_init(sqlite3 *db, char **UNUSED(pzErrMsg), const sqlite3_api_routines *pApi)
+{
+    SQLITE_EXTENSION_INIT2(pApi)
+    sqlite3_create_function(db, "levenshtein", 2, SQLITE_ANY, 0, levenshtein, 0, 0);
+    return SQLITE_OK;
+}
 
-static void levenFunc(sqlite3_context *context, int argc, sqlite3_value **argv)
+// The levenshtein sqlite function
+static void levenshtein(sqlite3_context *context, int UNUSED(argc), sqlite3_value **argv)
 {
     if (sqlite3_value_type(argv[0]) == SQLITE_NULL || sqlite3_value_type(argv[1]) == SQLITE_NULL){
         sqlite3_result_null(context);
         return;
     }
 
-    int result = levenshtein_distance((char*) sqlite3_value_text(argv[0]), (char*) sqlite3_value_text(argv[1]));
+    char const * const restrict str1 = (char const *) sqlite3_value_text(argv[0]);
+    char const * const restrict str2 = (char const *) sqlite3_value_text(argv[1]);
+    int result = levenshtein_distance(str1, str2);
 
     if (result == -1) {
-        // one argument too long
         sqlite3_result_null(context);
         return;
     }
@@ -30,14 +44,8 @@ static void levenFunc(sqlite3_context *context, int argc, sqlite3_value **argv)
     sqlite3_result_int(context, result);
 }
 
-int sqlite3_extension_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi)
-{
-    SQLITE_EXTENSION_INIT2(pApi)
-    sqlite3_create_function(db, "levenshtein", 2, SQLITE_ANY, 0, levenFunc, 0, 0);
-    return 0;
-}
-
-int levenshtein_distance(char *s1, char *s2)
+// The distance computation itself
+static inline int levenshtein_distance(char const * const restrict s1, char const * const restrict s2)
 {
     int m = strlen(s1);
     int n = strlen(s2);
@@ -46,27 +54,31 @@ int levenshtein_distance(char *s1, char *s2)
         return -1;
     }
 
-    if (m != 0 && n != 0) {
-        n += 1;
-        m += 1;
-        int *d = (int *) malloc((sizeof(int)) * n * m);
-        for (int k = 0; k < m; k++) {
-            d[k] = k;
-        }
-        for (int k = 0; k < n; k++){
-            d[k * m] = k;
-        }
-        for (int i = 1; i < m; i++){
-            for (int j = 1; j < n; j++){
-                int cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
-                d[j * m + i] = MIN(MIN(d[(j - 1) * m + i] + 1, d[j * m + i - 1] + 1), d[(j - 1) * m + i - 1] + cost);
-            }
-        }
-        int result = d[m * n - 1];
-        free(d);
-        return result;
-    } else {
-        return MAX(m, n);
+    if (m == 0 || n == 0) {
+        return m > n ? m : n;
     }
+
+    m += 1;
+    n += 1;
+
+    int *d = (int *) malloc(sizeof(int) * (n * m));
+
+    for (int k = 0; k < m; k++) {
+        d[k] = k;
+    }
+    for (int k = 0; k < n; k++){
+        d[k * m] = k;
+    }
+
+    for (int i = 1; i < m; i++){
+        for (int j = 1; j < n; j++){
+            int cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
+            d[j * m + i] = MIN(MIN(d[(j - 1) * m + i] + 1, d[j * m + i - 1] + 1), d[(j - 1) * m + i - 1] + cost);
+        }
+    }
+
+    int result = d[m * n - 1];
+    free(d);
+    return result;
 }
 
